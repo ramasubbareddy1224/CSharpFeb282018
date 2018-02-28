e96cd67ceb9d0f8aad9c66dbee8937b3f174fa30
ramasubba.m <ramasubba.m@cgi.com>
2018-02-28 08:24:45 +0530

    feb 28

---
+++ CSharpFeb2018/CSharpFeb2018/FirstProg.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Pragim.CSharp
+{
+    class FirstProg
+    {
+        static void Main()
+        {
+            Console.WriteLine("This is first program");
+
+            Console.Read();
+        }
+    }
+}
