a7418c68d86da3b676479ff6f198e92d73cdf0e7
ramasubba.m <ramasubba.m@cgi.com>
2018-03-02 08:25:18 +0530

    mar 2

--- CSharpFeb2018/CSharpFeb2018/FirstProg.cs
+++ CSharpFeb2018/CSharpFeb2018/FirstProg.cs
@@ -7,6 +7,17 @@ namespace Pragim.CSharp
         {
             Console.WriteLine("This is first program");
 
+            int number = 100;
+            double decimalnumber = 222.34;
+            string stringtext = "pragim";
+            char charactertext = 'A';
+            bool booltype = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine("mycharactertext is:" + charactertext + " and this is charcter type");
+            Console.WriteLine(string.Format("mycharactertext is:{0} and this is charcter type", charactertext));
+            Console.WriteLine(string.Format("number is:{0} and decimal number is:{1}", number, decimalnumber));
+
             Console.Read();
         }
     }
---
+++ CSharpFeb2018/CSharpFeb2018/TypeConversion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFeb2018
+{
+    class TypeConversion
+    {
+        static void Main()
+        {
+            // ------------- Boxing --------------- // 
+
+            int num = 10;  // value type
+            double score = 83.2;
+
+            string stringtext = num.ToString(); // Boxing : value type - ref type
+            string stringdouble = score.ToString();
+
+            Console.WriteLine(stringtext);
+            Console.WriteLine(stringdouble);
+
+            // -------------- ------------------- //
+
+            // ----------- UnBoxing -------------- //
+            string s = "100";
+            string s2 = "46.3";
+            string s3 = "true";
+            string s4 = "a";
+
+            int intvalue = Convert.ToInt32(s);
+            double dvalue = Convert.ToDouble(s2);
+            bool bvalue = Convert.ToBoolean(s3);
+            char cval = Convert.ToChar(s4);
+
+            Console.WriteLine(intvalue);
+            Console.WriteLine(dvalue);
+            Console.WriteLine(bvalue);
+            Console.WriteLine(cval);
+
+            Console.Read();
+
+        }
+    }
+}
